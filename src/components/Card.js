08b46ab343c53e2ac0b66afba07d75657ee630b3
GiacomoDiFa <giacomodfabrizio@gmai.com>
2023-02-17 16:04:06 +0100

    second commit

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Card() {
+    return (
+        <>
+            <div className="card h-100 shadow">
+                <div className="card-body">
+                    <h5 className="card-title text-start">Special title treatment</h5>
+                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card
