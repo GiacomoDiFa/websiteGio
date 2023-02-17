e066ee1aac733513a2c5c756e83dabcbe57b5ccc
GiacomoDiFa <73546258+GiacomoDiFa@users.noreply.github.com>
2023-02-17 16:10:36 +0100

    Add files via upload

---
+++ src/components/Form.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <>
+            <div class="mb-3 mt-3 shadow">
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="L'oggetto del messaggio?"/>
+            </div>
+            <div class="mb-3 shadow">
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="10" placeholder='Cosa vuoi dirmi?'></textarea>
+            </div>
+            <div className='mb-3'>
+                <button className='btn btn-primary'>Inviami un'email!</button>
+            </div>
+        </>
+    )
+}
+
+export default Form
--- src/pages/Homescreen.js
+++ src/pages/Homescreen.js
@@ -2,12 +2,18 @@ import React from 'react'
 import Card from '../components/Card'
 import Cardimage from '../components/Cardimage'
 import profilephoto from '../assets/giorgia.jpg';
+import Form from '../components/Form';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 function Homescreen() {
     return (
         <>
             <div className='section'>
-                <div className='container'>
+                <div className='container pt-5' data-aos="fade-up"data-aos-delay="500"
+    data-aos-duration="1000">
                     <div className='row no-gutters vh-100'>
                         <div className='col-lg-6 col-12 px-0 pb-5'>
                             <div className='container h-75 d-flex flex-column justify-content-center align-items-center mt-5'>
@@ -23,17 +29,22 @@ function Homescreen() {
                 </div>
             </div>
             <div className='section vh-100'>
-                <div className='container mb-5 mb-sm-3 mt-5'>
-                    <div className='row no-gutters'>
-                        <div className='col-lg-6 col-12'>
+                <div className='container mb-5 mb-sm-3 mt-5' data-aos="fade-right"data-aos-delay="500"
+    data-aos-duration="1000">
+                    <div className='row no-gutters' data-aos="fade-right"data-aos-delay="500"
+    data-aos-duration="1000">
+                        <div className='col-lg-6 col-12'data-aos="fade-right"data-aos-delay="500"
+    data-aos-duration="1000">
                             <h1>Condizioni di cui mi occupo</h1>
                         </div>
-                        <div className='col-lg-6 col-12 pt-3 pt-lg-0'>
+                        <div className='col-lg-6 col-12 pt-3 pt-lg-0'data-aos="fade-right"data-aos-delay="500"
+    data-aos-duration="1000">
                             <button className='btn btn-primary float-lg-end float-xs-start'>Contattami</button>
                         </div>
                     </div>
                 </div>
-                <div className='container h-50 mt-5'>
+                <div className='container h-50 mt-5' data-aos="fade-right"data-aos-delay="500"
+    data-aos-duration="1000">
                     <div className='row no-gutters h-100'>
 
                         <div className='col-sm-6 col-12 mb-3 mb-sm-3'>
@@ -51,24 +62,60 @@ function Homescreen() {
                     </div>
                 </div>
             </div>
-            <div className='section vh-100'>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col-lg-6 col-12'>
-                            <h1>I miei servizi</h1>
-                        </div>
-                        <div className='col-lg-6 col-12 pt-3 pt-lg-0'>
-                            <button className='btn btn-primary float-lg-end float-xs-start'>Contattami</button>
-                        </div>
+            <div className='container'data-aos="fade-right"data-aos-delay="500"
+    data-aos-duration="1000">
+                <div className='row'>
+                    <div className='col-lg-6 col-12'data-aos="fade-right"data-aos-delay="500"
+    data-aos-duration="1000">
+                        <h1>I miei servizi</h1>
+                    </div>
+                    <div className='col-lg-6 col-12 pt-3 pt-lg-0'data-aos="fade-right"data-aos-delay="500"
+    data-aos-duration="1000">
+                        <button className='btn btn-primary float-lg-end float-xs-start'>Contattami</button>
                     </div>
                 </div>
-                <div className='container h-75'>
-                    <div className='row h-100'>
-                        <div className='col-lg-6 col-12 h-100 mt-5 mt-lg-0'>
-                            <Cardimage/>
+            </div>
+            <div className='container h-75'data-aos="fade-right"data-aos-delay="500"
+    data-aos-duration="1000">
+                <div className='row h-100'>
+                    <div className='col-lg-6 col-12 h-100 mt-5 mt-lg-0'data-aos="fade-right"data-aos-delay="500"
+    data-aos-duration="1000">
+                        <Cardimage />
+                    </div>
+                    <div className='col-lg-6 col-12 h-100'data-aos="fade-right"data-aos-delay="500"
+    data-aos-duration="1000">
+                        <Cardimage />
+                    </div>
+                </div>
+            </div>
+
+            <div className='section vh-100'>
+                <div className='container pt-5'data-aos="fade-right"data-aos-delay="500"
+    data-aos-duration="1000">
+                    <div className='row'>
+                        <h1>I miei contatti</h1>
+                    </div>
+                </div>
+                <div className='container'data-aos="fade-right"data-aos-delay="500"
+    data-aos-duration="1000">
+                    <div className='row'>
+                        <div className='col-lg-6 col-12'data-aos="fade-right"data-aos-delay="500"
+    data-aos-duration="1000">
+                            <p className='mb-0 mt-3'>I miei studi:</p>
+                            <p className='fs-2 pt-0 mt-0'>Rimini (RN), Morciano di Romagna (RN), Montecalvo in Foglia (PU)</p>
+                            <hr></hr>
+                            <p className='mb-0'>Ricevo anche:</p>
+                            <p className='fs-2'>On-line</p>
+                            <hr></hr>
+                            <p className='mb-0'>Il mio indirizzo email:</p>
+                            <p className='fs-2'>giorgia.traversi@gmail.com</p>
+                            <hr></hr>
+                            <p className='mb-0'>Seguimi sui social:</p>
+                            <p>Icona insta, Icona fb</p>
                         </div>
-                        <div className='col-lg-6 col-12 h-100'>
-                            <Cardimage/>
+                        <div className='col-lg-6 col-12'data-aos="fade-right"data-aos-delay="500"
+    data-aos-duration="1000">
+                            <Form />
                         </div>
                     </div>
                 </div>
